17a792553e14c08d828c71e1161794f217c72542
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-02-04 22:57:25 -0700

    adds charsReducer key to mstp in view

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -6,8 +6,8 @@ import { CharacterList } from "../components";
 import { fetchData } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
@@ -16,24 +16,37 @@ class CharacterListView extends React.Component {
   }
 
   render() {
-    { this.props.error !== "" ? <p>this.props.error</p> : null }
-    { this.props.loading ? <h2>LOADING</h2> : null }
+    if (this.props.fetching) {
+      // return something here to indicate that you are fetching data
+      return <h2>LOADING</h2>;
+    }
+    console.log(this.props.characters)
     return (
       <div className="CharactersList_wrapper">
-        <h2>Star Wars Characters:</h2>
+        <h1>Star Wars Characters:</h1>
         <CharacterList characters={this.props.characters} />
       </div>
     );
-  } 
+  }
+  // render() {
+  //   // return (
+  //   //   <div className="CharactersList_wrapper">
+  //   //     { this.props.error !== "" ? <p>this.props.error</p> : null }
+  //   //     { this.props.loading ? <h2>LOADING</h2> : null }
+  //   //     <h2>Star Wars Characters:</h2>
+  //   //     <CharacterList characters={this.props.characters} />
+  //   //   </div>
+  //   // );
+  // } 
 }
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 const mstp = state => {
   return { 
-    characters: state.characters,
-    fetching: state.fetching,
-    error: state.error
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching,
+    error: state.charsReducer.error
   }
 }
-export default connect( mstp,{ fetchData: fetchData })(CharacterListView);
+export default connect( mstp, { fetchData: fetchData } )( CharacterListView );
